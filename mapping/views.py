ffcce46be22ab6a23693cfe90218f937bbf57954
avyavkumar <avyavkumar@gmail.com>
2015-07-02 13:41:21 +0530

    integration with database of task and dept

--- mapping/views.py
+++ mapping/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render
 from mapping.forms import CountryForm, DepartmentForm, TaskForm, SubtaskForm, DescriptionForm
 from mapping.models import Country, Department, Task, Subtask, Description
+from django.template import Library
+
 # Create your views here.
+
+register = Library
 
 def home(request):
     return render(request,'mapping/home.html',{'title':'HOME'})
 
 def department(request):
-    return render(request,'mapping/departments.html',{'title':'DEPARTMENTS'})
+    departments = Department.objects.all().order_by('name')
+    d = Department()
+    tasks = Task.objects.all().order_by('name')
+    t = Task()
+    # total_depts = sum(0 == 0 for department in departments)
+    # total_tasks = sum(0 == 0 for task in tasks)
+    return render(request,'mapping/departments.html',{'departments': departments, 'title':'Departments', 'tasks': tasks})
+
+def tasks(request, department=None, task=None):
+    department = Department.objects.get(id=department)
+    task = Task.objects.get(id=task)
+    d = department.id
+    t = task.id
+    departments = Department.objects.all().order_by('name')
+    tasks = Task.objects.all().order_by('name')
+    subtasks = Subtask.objects.all().order_by('name')
+    return render(request,'mapping/viewtask.html',{'tasks' : tasks, 'departments': departments, 'subtasks': subtasks, 'd' : d, 't' : t})
+
+def subtasks(request, department=None, task=None, subtask=None):
+    # department = Department.objects.get(id=department)
+    # task = Task.objects.get(id=task)
+    # d = department.id
+    # t = task.id
+    # departments = Department.objects.all().order_by('name')
+    # tasks = Task.objects.all().order_by('name')
+    # subtasks = Subtask.objects.all().order_by('name')
+    # return render(request,'mapping/viewtask.html',{'tasks' : tasks, 'departments': departments, 'subtasks': subtasks, 'd' : d, 't' : t})
+    pass
 
 def newframework(request):
     return render(request,'mapping/newframework.html',{'title':'NS in NEW FRAMEWORK'})
--- nsmm/urls.py
+++ nsmm/urls.py
@@ -6,10 +6,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nsmm.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',views.home),
     url(r'^department$',views.department),
+    url(r'^department/(?P<department>\d+)/task/(?P<task>\d+)$',views.tasks),
+    url(r'^department/(?P<department>\d+)/task/(?P<task>/subtask/(?P<subtask>)\d+)$',views.subtasks),
     url(r'^od/newframework$',views.newframework),
     url(r'^od/nsd$',views.nsd),
     url(r'^od/youth$',views.youth),
