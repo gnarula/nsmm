1c6b6b8d92e67a83150e4ee42d300e92fd11f691
avyavkumar <avyavkumar@gmail.com>
2015-07-06 13:29:32 +0530

    login page implemented, models updated

--- mapping/migrations/0001_initial.py
+++ mapping/migrations/0001_initial.py
@@ -7,38 +7,91 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
         migrations.CreateModel(
+            name='CustomUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('password', models.CharField(verbose_name='password', max_length=128)),
+                ('last_login', models.DateTimeField(verbose_name='last login', null=True, blank=True)),
+                ('is_superuser', models.BooleanField(verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.', default=False)),
+                ('first_name', models.CharField(max_length=50)),
+                ('last_name', models.CharField(max_length=50)),
+                ('email', models.EmailField(unique=True, max_length=254)),
+                ('is_active', models.BooleanField(default=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
             name='Country',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('name', models.CharField(max_length=50)),
             ],
         ),
         migrations.CreateModel(
-            name='TaskDesc',
+            name='Department',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('date', models.DateField()),
-                ('description', models.CharField(max_length=500)),
-                ('status', models.IntegerField(choices=[(1, 'not started'), (2, 'initial stages'), (3, 'intermediate'), (4, 'advanced'), (5, 'complete')])),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('name', models.CharField(max_length=50)),
             ],
         ),
         migrations.CreateModel(
-            name='TaskDir',
+            name='Description',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('department', models.CharField(choices=[('Health', 'H'), ('Organisation Development', 'OD'), ('Disaster Management', 'DM')], max_length=40)),
-                ('task', models.CharField(max_length=100)),
-                ('subtask', models.CharField(max_length=100)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('created_at', models.DateTimeField(auto_now=True)),
+                ('description', models.CharField(max_length=500)),
+                ('status', models.IntegerField(choices=[(1, 'Not started'), (2, 'Initial Stages'), (3, 'Intermediate'), (4, 'Advanced'), (5, 'Complete')])),
                 ('country', models.ForeignKey(to='mapping.Country')),
+            ],
+            options={
+                'ordering': ('-created_at',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Subtask',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('name', models.CharField(max_length=50)),
+                ('department', models.ForeignKey(to='mapping.Department')),
             ],
         ),
         migrations.AddField(
-            model_name='taskdesc',
-            name='task_dir',
-            field=models.ForeignKey(to='mapping.TaskDir'),
+            model_name='subtask',
+            name='task',
+            field=models.ForeignKey(to='mapping.Task'),
+        ),
+        migrations.AddField(
+            model_name='description',
+            name='subtask',
+            field=models.ForeignKey(to='mapping.Subtask'),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='country',
+            field=models.ForeignKey(to='mapping.Country'),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='groups',
+            field=models.ManyToManyField(to='auth.Group', related_query_name='user', related_name='user_set', blank=True, verbose_name='groups', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        ),
+        migrations.AddField(
+            model_name='customuser',
+            name='user_permissions',
+            field=models.ManyToManyField(to='auth.Permission', related_query_name='user', related_name='user_set', blank=True, verbose_name='user permissions', help_text='Specific permissions for this user.'),
         ),
     ]
--- mapping/models.py
+++ mapping/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class Country(models.Model):
     name = models.CharField(max_length=50)
@@ -11,6 +12,74 @@ class Department(models.Model):
 
     def __str__(self):
         return self.name
+
+class UserManager(BaseUserManager):
+    def create_user(self,
+        email,
+        first_name,
+        last_name,
+        country,
+        password
+    ):
+        """
+        Creates a new user
+        """
+
+        if not email:
+            raise ValueError('User must have a valid email address')
+
+        c = Country.objects.get(pk=country)
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            country=c
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        email,
+        first_name,
+        last_name,
+        country,
+        password
+    ):
+        """
+        Creates a new superuser
+        """
+
+
+        user = self.create_user(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            country=country,
+            password=password,
+        )
+
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    country = models.ForeignKey(Country)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True, null=False, blank=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'first_name',
+        'last_name',
+        'country',
+    ]
 
 class Task(models.Model):
     department = models.ForeignKey(Department)
